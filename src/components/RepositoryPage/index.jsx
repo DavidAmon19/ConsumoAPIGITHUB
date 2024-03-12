2c051a3d4573d323c597cdbcea2a17b0a38f5df2
Marcos Aurelio <127764997+marcosaureliosl@users.noreply.github.com>
2024-03-12 12:08:28 -0300

    Revert "Criação do component styled"

--- src/components/RepositoryPage/index.jsx
+++ src/components/RepositoryPage/index.jsx
@@ -2,7 +2,61 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
-import {PageContainer,  Avatar , Title, RepositoriesList, BackButton, } from "./styles";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 12px;
+    color: #FFF;
+  
+`;
+
+const Avatar = styled.img`
+  width: 100px; 
+  height: 100px; 
+  border-radius: 50%;
+  margin-bottom: 5px;
+  border: 2px solid #e5f01084;
+`;
+
+const Title = styled.h1`
+  background-color: #e5f01084;
+  padding: 8px;
+  margin-bottom: 8px;
+  font-size: 20px;
+`;
+
+const RepositoriesList = styled.div`
+  background-color: #e5f01084;
+  padding: 15px;
+
+  h2 {
+    font-size: 18px;
+  }
+
+  p {
+    color: #433db9;
+    font-weight: bold;
+    font-size:12px;
+  }
+`;
+
+const BackButton = styled(Link)`
+  background-color: #4f46e5;
+  color: #fff;
+  padding: 10px 20px;
+  text-decoration: none;
+  font-weight: bold;
+  border-radius: 5px;
+  margin-top: 10px;
+  transition: background-color 0.3s ease; 
+
+  &:hover {
+    background-color: #3c3480;
+  }
+`;
 
 export const RepositoryPage = () => {
   const { username } = useParams();
